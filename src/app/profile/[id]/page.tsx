bd60c3b3ad945f4b0f890e59d72cd24eec0c871e
Sai Charan Reddy Puligari <reddysaicharan12345@gmail.com>
2024-10-28 18:13:15 +0530

    ts errors

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,22 +1,27 @@
+import { NextPage } from 'next';
+
 interface Params {
     params: {
-      id: string;
+        id: string;
     };
-  }
-export default function userProfile({params}:Params){
+}
 
+// Use NextPage to properly type the component
+const UserProfile: NextPage<Params> = ({ params }) => {
     return (
         <div className="flex items-center justify-center min-h-screen py-2 bg-gray-200">
-      <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-4">Profile</h1>
-        <hr className="mb-4" />
-        <p className="text-lg">
-          Profile Page 
-          <span className="p-2 ml-2 rounded bg-orange-500 text-black">
-            {params.id}
-          </span>
-        </p>
-      </div>
-    </div>
-    )
-}
+            <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full">
+                <h1 className="text-2xl font-bold mb-4">Profile</h1>
+                <hr className="mb-4" />
+                <p className="text-lg">
+                    Profile Page 
+                    <span className="p-2 ml-2 rounded bg-orange-500 text-black">
+                        {params.id}
+                    </span>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default UserProfile;
